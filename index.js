4fd83e0e133710bcde177374aaa17cb931a5cce3
Javier Ferrante <javierferrante@gmail.com>
2016-02-24 19:28:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ transporter.sendMail(mailOptions, function(error, info){
 	}
 });
 
-var server = app.listen(3009, function () {
+var server = app.listen(process.env.PORT, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Example app listening at http://%s:%s', host, port);
